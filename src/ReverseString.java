9d0c53e127d4a019e1f9d76899098e8c2041ba5f
suvarnashende <suvarnashende04@gmail.com>
2024-05-02 17:52:44 +0530

    adding a code

---
+++ src/ReverseString.java
@@ -0,0 +1,12 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String v = "Nitin";
+		for (int i =v.length()-1; i>=0; i--) {
+		
+		      System.out.print(v.charAt(i));
+		}
+	}
+
+}
