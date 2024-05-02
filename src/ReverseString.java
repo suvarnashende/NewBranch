4f25a3d1d698e86e33e5661a3eb26ac857ff4f6b
Pallavi20241 <pikudjadhav@gmail.com>
2024-05-02 18:09:38 +0530

    mumbai

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -2,7 +2,7 @@
 public class ReverseString {
 
 	public static void main(String[] args) {
-		String v = "Nitin";
+		String v = "mumbai";
 		for (int i =v.length()-1; i>=0; i--) {
 		
 		      System.out.print(v.charAt(i));
